47c45752764192caa86dabc98958a6c0cc852dbe
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-30 06:43:56 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,4 +1,6 @@
 module Memorable
+  
+  module ClassMethods
   def reset_all
     self.all.clear
   end
@@ -6,4 +8,11 @@ module Memorable
   def count
     self.all.count
   end
+end
+  
+  module InstanceMethods
+    def initialize
+  self.class.all << self
+end
+end
 end
